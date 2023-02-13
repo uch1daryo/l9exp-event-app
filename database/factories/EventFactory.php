c32909851647487f5280bb8cd2af0150c088edd5
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-02-13 13:36:50 +0900

    feat: added User factory and Event factory

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
